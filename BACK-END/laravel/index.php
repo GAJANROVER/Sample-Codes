4797262d2fe43b59ef71c5e19746bbb3bd843e7e
Gajan <sivagajan96in@gmail.com>
2024-03-04 12:23:07 +0530

    added laravel folder

---
+++ BACK-END/laravel/index.php
@@ -0,0 +1,6 @@
+<?php  
+
+// laravel codes
+// laravel new my_first_app   - creating new app with name
+// php artisan serve   - run the server 
+// php artisan --version   - check laravel version 
