6188b4579725a1c093912bce0bf89542d03c8d8a
Gajan <sivagajan96in@gmail.com>
2024-03-06 10:33:38 +0530

    added laravel commands

--- BACK-END/laravel/index.php
+++ BACK-END/laravel/index.php
@@ -4,3 +4,5 @@
 // laravel new my_first_app   - creating new app with name
 // php artisan serve   - run the server 
 // php artisan --version   - check laravel version 
+// php artisan migrate   -  database migration
+// php artisan make:controller [controler Name]  - create controller
