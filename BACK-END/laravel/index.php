0bcc980b3181e09f03ff2812db5fe21c17755bc5
Gajan <sivagajan96in@gmail.com>
2024-06-04 08:51:44 +0530

    added

--- BACK-END/laravel/index.php
+++ BACK-END/laravel/index.php
@@ -6,3 +6,6 @@
 // php artisan --version   - check laravel version 
 // php artisan migrate   -  database migration
 // php artisan make:controller [controler Name]  - create controller
+// php artisan make:migration create_flights_table - create migration file
+// php artisan make:model UploadedFile - create model
+// php artisan make:migration create_uploaded_files_table --create=uploaded_files
